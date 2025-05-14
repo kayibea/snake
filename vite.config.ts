2908e3dc76819e03d5cc5fb62c0ff77561255ef2
kayibea <gatse1531@gmail.com>
2025-05-14 05:10:31 +0200

    🔧 update config

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import path from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/sssssssurvivor',
+  base: '/snake',
   resolve: {
     alias: {
       utils: path.resolve(__dirname, 'src/utils'),
